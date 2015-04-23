7f79009f54aaebf33ab8b93bf674770c7298ea40
Massimiliano Arione <garakkio@gmail.com>
2015-04-23 15:50:18 +0200

    add default listener for fk exceptions

---
+++ src/AppBundle/Listener/KernelExceptionListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+/**
+ * Intercept an exception related to FK constraint violation is thrown, add a flash
+ * message and return to previous page
+ */
+class KernelExceptionListener
+{
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        if ($exception instanceof ForeignKeyConstraintViolationException) {
+            $request = $event->getRequest();
+            $request->getSession()->getFlashBag()->add('danger', 'Cannot delete this element, it has relations.');
+            $response = new RedirectResponse($request->headers->get('referer'));
+            $event->setResponse($response);
+        }
+    }
+}
