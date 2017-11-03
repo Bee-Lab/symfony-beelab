14a5502113c79c6bd9e44f6fe609859b1492ce47
Massimiliano Arione <garakkio@gmail.com>
2017-11-03 10:36:18 +0100

    fix bundles

--- config/bundles.php
+++ config/bundles.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
-    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
-    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
-    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
-    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
-    'Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle'  => ['all' => true],
-    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle'  => ['all' => true],
-    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle'  => ['all' => true],
-    'Sensio\Bundle\DistributionBundle\SensioDistributionBundle'  => ['dev' => true, 'test' => true],
-    'Beelab\TestBundle\BeelabTestBundle'  => ['test' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Sensio\Bundle\DistributionBundle\SensioDistributionBundle::class => ['dev' => true, 'test' => true],
+    Beelab\TestBundle\BeelabTestBundle::class => ['test' => true],
 ];
