7d0a28dc0cd2bc64b7278b2808729901407bebfd
Massimiliano Arione <garakkio@gmail.com>
2018-08-10 17:55:37 +0200

    upgrade vendors

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Beelab\TestBundle\BeelabTestBundle::class => ['test' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 final class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
-     * @Method("GET")
+     * @Route("/", name="homepage", methods={"GET"})
      */
     public function homepage(): Response
     {
