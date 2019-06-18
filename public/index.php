6f029bd7d2037defd9c6551c135fd3ccbf6d9767
Massimiliano Arione <garakkio@gmail.com>
2019-06-18 14:57:46 +0200

    update recipes

--- public/index.php
+++ public/index.php
@@ -7,12 +7,20 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__.'/../vendor/autoload.php';
 
 if ($_SERVER['APP_DEBUG'] ?? false) {
+    umask(0000);
+
     Debug::enable();
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', $_SERVER['APP_DEBUG'] ?? false);
-//Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_FORWARDED);
-//Request::enableHttpMethodParameterOverride();
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
