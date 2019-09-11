290a948dc48049eb253f07c199910db02c66772c
Massimiliano Arione <garakkio@gmail.com>
2019-09-11 17:43:53 +0200

    assets fix

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
     .addStyleEntry('css/app', './assets/scss/app.scss')
 
     // allow sass/scss files to be processed
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // allow legacy applications to use $/jQuery as a global variable
     .autoProvidejQuery()
