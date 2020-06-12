4592f329be8e67fc92969fbb4fe8ce1cd1e6600a
Massimiliano Arione <garakkio@gmail.com>
2020-06-12 11:36:17 +0200

    ⬆️  aggiorna vendor

--- src/App/Kernel.php
+++ src/App/Kernel.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
@@ -13,12 +12,10 @@ final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
-
     /**
-     * @return \Iterator|\Symfony\Component\HttpKernel\Bundle\BundleInterface[]
+     * @return iterable|\Symfony\Component\HttpKernel\Bundle\BundleInterface[]
      */
-    public function registerBundles(): \Iterator
+    public function registerBundles(): iterable
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
@@ -30,21 +27,19 @@ final class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
         $container->setParameter('container.dumper.inline_class_loader', $this->debug);
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir().'/config';
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{packages}/*.yaml', 'glob');
+        $loader->load($confDir.'/{packages}/'.$this->environment.'/*.yaml', 'glob');
+        $loader->load($confDir.'/services.yaml');
+        $loader->load($confDir.'/{services}_'.$this->environment.'.yaml', 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
         $confDir = $this->getProjectDir().'/config';
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir.'/{routes}/'.$this->environment.'/*.yaml', '/', 'glob');
+        $routes->import($confDir.'/routes.yaml');
     }
 }
