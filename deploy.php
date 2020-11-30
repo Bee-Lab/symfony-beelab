a41babcdb2c119f9dd0988c6837984a2e589e76a
Massimiliano Arione <garakkio@gmail.com>
2020-11-30 16:44:06 +0100

    🔧 fix versione xdebug

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ namespace Deployer;
 require 'recipe/symfony.php';
 
 set('application', 'customize');
-set('repository', 'git@bitbucket.org:garak/customize.git');
+set('repository', 'git@bitbucket.org:garak/{{application}}.git');
 set('git_tty', true);
 set('keep_releases', 2);
 set('shared_files', []);
@@ -17,23 +17,36 @@ set('release_name', static function (): string {
 
 desc('Update database');
 task('deploy:db:update', static function (): void {
-    $url = run('grep APP_DATABASE_URL /etc/nginx/sites-available/customize');
-    preg_match('/(mysql:\/\/.*[^;])/', $url, $matches);
+    $url = run('grep APP_DATABASE_URL /etc/nginx/sites-available/{{application}}');
+    \preg_match('/(mysql:\/\/.*[^;])/', $url, $matches);
     run('APP_DATABASE_URL=\''.$matches[0].'\' {{bin/console}} doctrine:schema:update --force {{console_options}}');
 });
 
 desc('Copy remote database to local');
 task('deploy:db:from_remote_copy', static function (): void {
-    $url = run('grep APP_DATABASE_URL /etc/nginx/sites-available/customize');
-    preg_match('/mysql:\/\/(\w+):(.+)@.+/', $url, $matches);
-    $name = '{{application}}_'.date('YmdHis').'.sql';
+    $url1 = run('grep APP_DATABASE_URL /etc/nginx/sites-available/{{application}}');
+    \preg_match('/mysql:\/\/(\w+):(.+)@.+/', $url1, $matches);
+    $name = '{{application}}_'.\date('YmdHis').'.sql';
     run('mysqldump -u '.$matches[1].' -p'.$matches[2].' {{application}} > /tmp/'.$name.'; gzip /tmp/'.$name);
     runLocally('scp {{user}}@{{hostname}}:/tmp/'.$name.'.gz .');
     $url2 = runLocally('env | grep APP_DATABASE_URL');
-    preg_match('/APP_DATABASE_URL=mysql:\/\/(\w+):(.+)@(.+)\/(.+)/', $url2, $matches);
+    \preg_match('/APP_DATABASE_URL=mysql:\/\/(\w+):(.+)@(.+)\/(.+)/', $url2, $matches);
     runLocally('gunzip '.$name.'.gz');
     runLocally('mysql -u'.$matches[1].' -p'.$matches[2].' -h'.$matches[3].' -D'.$matches[4].' < '.$name);
     runLocally('rm '.$name);
+});
+desc('Copy local database to remote');
+task('deploy:db:to_remote_copy', static function (): void {
+    $url1 = runLocally('env | grep APP_DATABASE_URL');
+    \preg_match('/mysql:\/\/(\w+):(.+)@(.+)\/(.+)/', $url1, $matches);
+    $name = '{{application}}_'.\date('YmdHis').'.sql';
+    runLocally('mysqldump -u '.$matches[1].' -p'.$matches[2].' -h'.$matches[3].' '.$matches[4].' > /tmp/'.$name.'; gzip /tmp/'.$name);
+    runLocally('scp /tmp/'.$name.'.gz {{user}}@{{hostname}}:/tmp/');
+    $url2 = run('grep APP_DATABASE_URL /etc/nginx/sites-available/{{application}}');
+    \preg_match('/APP_DATABASE_URL mysql:\/\/(\w+):(.+)@(.+)\/(.+);/', $url2, $matches);
+    run('gunzip /tmp/'.$name.'.gz');
+    run('mysql -u'.$matches[1].' -p'.$matches[2].' -h'.$matches[3].' -D'.$matches[4].' < /tmp/'.$name);
+    run('rm /tmp/'.$name);
 });
 desc('Precompile assets');
 task('deploy:assets:build_local', static function (): void {
@@ -46,21 +59,17 @@ desc('Upload precompiled assets');
 task('deploy:assets:upload', static function (): void {
     runLocally('scp public/build/assets.tgz {{user}}@{{hostname}}:{{deploy_path}}');
     run('mv {{deploy_path}}/assets.tgz {{release_path}}; cd {{release_path}}; tar zxvf assets.tgz; rm assets.tgz');
+    runLocally('rm public/build/assets.tgz');
 });
 
 host('production')
-    ->setHostname('contabo')
-    ->set('deploy_path', '~/sf/{{application}}')
-    ->set('user', 'garak')
+    ->setHostname('myhost') // TODO set your hostname
+    ->set('deploy_path', '~/{{application}}')
+    ->set('user', 'myuser') // TODO set your username
 ;
 
 task('deploy:update_code', static function (): void {
     run('git archive --remote {{repository}} --format=tar {{branch}} | (cd {{release_path}} && tar xf -)');
-});
-// TODO remove this task after new release (post-beta3)
-// see https://github.com/deployphp/deployer/issues/2244
-task('success', static function (): void {
-    info('successfully deployed!');
 });
 
 after('deploy:failed', 'deploy:unlock');
