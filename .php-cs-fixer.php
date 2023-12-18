7241ae2c67b15b69525789f4e8d46f26270f06e9
Massimiliano Arione <garakkio@gmail.com>
2023-12-18 08:51:48 +0100

    Symfony 6

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,7 +15,6 @@ return (new PhpCsFixer\Config())
         'declare_strict_types' => false,
         'native_function_invocation' => ['include' => ['@all']],
         'method_chaining_indentation' => true,
-        'phpdoc_trim_consecutive_blank_line_separation' => true,
         'fopen_flags' => ['b_mode' => true],
         'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'match', 'parameters']],
     ])
